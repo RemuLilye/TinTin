b6418c71c506c3eb5d3f4c79a04de6259b91acfd
{SERIA ORION} <jacklupin@icloud.com>
2024-02-04 18:39:36 -0500

    Initial Commit

---
+++ TinTin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TinTin
+//
+//  Created by SeriaThassleRemuLilyeNazarekMeku on 2/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TinTin/TinTinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinTinApp.swift
+//  TinTin
+//
+//  Created by SeriaThassleRemuLilyeNazarekMeku on 2/4/24.
+//
+
+import SwiftUI
+
+@main
+struct TinTinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
